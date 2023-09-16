4419598802df6136deb1de567d3178a00fba0623
rcompanocca <ronicompanocca@gmail.com>
2023-09-16 10:01:10 -0500

    avance

--- hanged.java
+++ hanged.java
@@ -1,9 +1,12 @@
 // RONI COMPANOCCA CHECCO
+// CUI: 20210558
 // LABORATORIO 01
+// FUNDAMENTOS DE PROGRAMACION 
 
 import java.util.Scanner;
 public class hanged {
     public static void main(String []args){
+    	
         String ahor1 = "  +---+ \n"+
                        "  |   | \n" + 
                        "      | \n" +
@@ -72,7 +75,58 @@ public class hanged {
         while(contador <= 6){
             letra = ingreseletra();
             if (letraEnPalabraSecreta(letra, palSecreta))
-                mostrarBlancosActualizado
+                mostrarBlancosActualizados(letra);
+            else {
+                System.out.println(figuras[contador]);
+            }
+            contador = contador + 1;
         }
+
+        // COMPLETAR PARA INDICAR SI GANO, PERDIO Y CUANTOS TURNOS NECESITO
+        System.out.println("\n");
+    }
+    
+    public static String getPalabraSecreta(String [] lasPalabras){
+        String palSecreta;
+        int ind;
+        int indiceMayor = lasPalabras.length -1;
+        int indiceMenor = 0;
+        ind = (int) ((Math.random()*(indiceMayor - indiceMenor +1)+ indiceMenor));
+        return lasPalabras[ind];
+    }
+    
+    public static void mostrarBlancos(String palabra){
+        for(int i=0; i<palabra.length(); i++){
+            System.out.print("- " );
+        }
+    }
+    
+    public static String ingreseletra(){
+        String laLetra;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ingrese letra: ");
+        laLetra = sc.next();
+        while(laLetra.length()!=1){
+            System.out.println("ingrese letra: "); // COMPLETAR PARA VALIDAR CARACTERES PERITIDOS
+            laLetra = sc.next();
+        }
+        return laLetra;
+    }
+    
+    public static boolean letraEnPalabraSecreta(String letra, String palSecreta){
+    	char[] letras = new char[palSecreta.length()];
+    	char letter = letra.charAt(0);
+    	for (int i = 0; i < letras.length; i++) 
+    		letras[i] = palSecreta.charAt(i);
+    	for (int j = 0; j < letras.length; j++) {
+    		if(letter == letras[j]);
+    	    return true;
+    	}
+    	return false;
+    }
+    
+    public static void mostrarBlancosActualizados(String letra){
+        //COMPLETAR
+        System.out.println("PROCESANDO......");
     }
 }
